17c6e7aaf897696b6885a2163890f5d34f854212
RobPerham <robperham15@gmail.com>
2024-06-13 13:10:42 +0100

    all working
    i just to improve on the location and get more info for site

--- index.js
+++ index.js
@@ -1,33 +1,58 @@
-const apikey =`b6b8a87e3965b8b3d3439101c358b0bb`;
+const apikey = "b6b8a87e3965b8b3d3439101c358b0bb";
 
-const main= document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
-async function getWeatherByLocation(city){
-    const resp = await fetch(url(city), {origin: "cors"});
-    const respData = await resp.json();
+async function getWeatherByLocation(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
 
-    addWeatherToPage(respData);
+  console.log(respData);
+
+  addWeatherToPage(respData);
 }
 
-function addWeatherToPage(data){
-    const temp = KtoC(data.main.temp);
-    const humidity = data.main.humidity;
-    const weather = document.createElement('div');
-    weather.classList.add('weather');
+function addWeatherToPage(data) {
+  const temp = KtoC(data.main.temp);
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
 
-    weather.innerHTML = `
-        <small>${data.name}</small>
-        <h2>${temp}°C</h2>
-        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+
+  weather.innerHTML = `
+        <h2><img src="https://openweathermap.org/img/wn/${
+          data.weather[0].icon
+        }@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${
+    data.weather[0].icon
+  }@2x.png" /></h2>
         <small>${data.weather[0].main}</small>
+        <div class="more-info">
+        <p>Humidity : <span>${humidity}%</span></p>
+        <p>Wind speed : <span>${+Math.trunc(windSpeed * 3.16)}km/h</span></p>
+        </div>
     `;
 
-    // cleanup
-    main.innerHTML = '';
+  // cleanup
+  main.innerHTML = "";
 
-    main.appendChild(weather);
+  main.appendChild(weather);
 }
+
+function KtoC(K) {
+  return Math.floor(K - 273.15);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+
+  if (city) {
+    getWeatherByLocation(city);
+  }
+});
